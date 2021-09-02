3d2989190fcb7111d4a57704d154b3f9d166d927
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-09-02 12:00:30 -0700

    added timer

--- script.js
+++ script.js
@@ -9,6 +9,7 @@
 // do-while
 
 // loop until we reach 10
+/*
 for (let idx = 10; idx >= 1; idx--)
 {
     // we have the code we want repeat
@@ -16,3 +17,44 @@ for (let idx = 10; idx >= 1; idx--)
 }
 // this line code will run when we exit the loop
 alert('Done with the loop thangy');
+*/
+
+//Even in a while loop we still have end condition
+
+// Classic user input for when to quit
+// let whileNotEddie = true  // flag
+
+// while (whileNotEddie)
+// {
+    // get name from user
+    // let coolName = prompt("Enter a cool name").toUpperCase();
+
+    //debug this with science
+    //alert(coolName);
+    // did they enter the name we wanted?
+    // if ( coolName === "Eddie") {
+        //   whileNotEddie = false;
+    // }
+// }
+
+// alert('Done with the while loop thingy');
+
+// keep asking the user for a cool name until they enter Eddie
+/*while (x <= 10)
+{
+    console.log(`Current iteration is ${x}`);
+    x++;
+}
+
+alert('Done with the while loop thingy');
+*/
+console.log('BREAK OFF THE ATTACK! Their shields are still up!');
+setTimeout(itsATrap, 30000) // call the ducntion itsatrap after 3 seconds
+
+// this is our callback function
+?*function itsATrap() 
+{
+   const msg = "IT'S A TRAP!"
+   console.log(msg);
+   alert(msg);
+}
