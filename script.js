93dea4b7cbb49796c2ad26102d2c1f24172dc698
Shanythia Richards <shanythia.m.richards@gmail.com>
2021-09-02 10:26:10 -0700

    milestone reached

---
+++ script.js
@@ -0,0 +1,18 @@
+// sanity test
+   // alert("lets go!");
+// learning loops   
+
+// for 
+   // for - when I know how many
+// while
+   // while - just keep doing it until i'm done
+// do-while
+
+// loop until we reach 10
+for (let idx = 10; idx >= 1; idx--)
+{
+    // we have the code we want repeat
+    console.log(`Current iteration is ${idx}`);
+}
+// this line code will run when we exit the loop
+alert('Done with the loop thangy');
